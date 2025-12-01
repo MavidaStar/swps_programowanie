c012c79fef6eafb40090afe7b0feee69345b4c5e
MavidaStar <martynamamczura@gmail.com>
2025-12-01 23:51:24 +0100

    Renew project after lost of pendrive

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,18 +2,26 @@
 string GetCorrectSign(string playerName)
 {
     Console.WriteLine($"{playerName}, choose your sign ({string.Join('/', allowedSigns)})");
-    string firstSign = Console.ReadLine()!;
+    string sign = Console.ReadLine()!;
 
     //if (firstSign.Equals("rock", stringComaprison) && !firstSign.Equals("paper", stringComaprison) && !firstSign.Equals("scissors", stringComaprison))
     //while (!(firstSign.Equals(allowedSigns[0], stringComparison) || firstSign.Equals(allowedSigns[1], stringComparison) || firstSign.Equals(allowedSigns[2], stringComparison)))
-    while (!allowedSigns.Contains(firstSign, StringComparer.OrdinalIgnoreCase))
+    while (!allowedSigns.Contains(sign, StringComparer.OrdinalIgnoreCase))
     {
         Console.WriteLine($"{playerName}, choose correct sign ({string.Join('/', allowedSigns)})");
-        firstSign = Console.ReadLine()!;
+        sign = Console.ReadLine()!;
     }
-    return firstSign;
+    return sign;
 }
 
+string GetCorrectRandomSign(string playerName)
+{
+    int signIndex = Random.Shared.Next(allowedSigns.Count);
+    string sign = allowedSigns[signIndex];
+    Console.WriteLine($"{playerName} selected {sign}");
+
+    return sign;
+}
 
 const StringComparison stringComparison = StringComparison.OrdinalIgnoreCase;
 
@@ -33,18 +41,31 @@ Console.WriteLine("Let's play Rock-Paper-Scissors!");
 //    firstSign = Console.ReadLine()!;
 //}
 string firstSign = GetCorrectSign("Player 1");
-string secondSign = GetCorrectSign("Player 2");
+string secondSign = GetCorrectRandomSign("Player 2");
+
+// 1. Pobierz indeks znaku podanego przez osobę drugą (np. 0, 1, 2) - nazwę to secondSignIndex
+int secondSignIndex = allowedSigns.IndexOf(secondSign);
+// 2. Wylicz indeks znaku, który wygrywa z podanym przez osobę drugą - wzór - (secondSignIndex + 1) % rozmiarListy
+int winningSignIndex = (secondSignIndex + 1) % allowedSigns.Count;
+// 3. Czy indeks znaku podanego przez pierwszą osobę to indeks wyliczony w punkcie 2.
+int firstSignIndex = allowedSigns.IndexOf(firstSign);
 
 if (firstSign.Equals(secondSign, stringComparison))
 {
     Console.WriteLine("It's a draw!");
 }
-else if ((firstSign.Equals(allowedSigns[0], stringComparison) && secondSign.Equals(allowedSigns[2], stringComparison))
-    || (firstSign.Equals(allowedSigns[1], stringComparison) && secondSign.Equals(allowedSigns[0], stringComparison))
-    || (firstSign.Equals(allowedSigns[2], stringComparison) && secondSign.Equals(allowedSigns[1], stringComparison)))
+else if (firstSignIndex == winningSignIndex)
 {
     Console.WriteLine("First player won!");
 }
+
+//else if ((firstSign.Equals(allowedSigns[0], stringComparison) && secondSign.Equals(allowedSigns[2], stringComparison))
+//    || (firstSign.Equals(allowedSigns[1], stringComparison) && secondSign.Equals(allowedSigns[0], stringComparison))
+//    || (firstSign.Equals(allowedSigns[2], stringComparison) && secondSign.Equals(allowedSigns[1], stringComparison)))
+//{
+//    Console.WriteLine("First player won!");
+//}
+
 else
 {
     Console.WriteLine("Second player won!");
